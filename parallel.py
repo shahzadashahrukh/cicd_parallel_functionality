4c989897cf6844e0a4ab7c1849e269b281804115
sshahruk <sshahruk@cisco.com>
2016-12-08 18:05:41 -0500

    Integrating Jenkins with GitHub and Local Machine

--- parallel.py
+++ parallel.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 
-print("Integrating Jenkins with GitHub")
-print("Integrating Jenkins with GitHub")
-print("Integrating Jenkins with GitHub")
-print("Integrating Jenkins with GitHub")
+print("Integrating Jenkins with GitHub and Local Machine")
