602794e628e9a72fdd5137a624388f1b7424f666
sshahruk <sshahruk@cisco.com>
2016-12-09 19:19:25 -0500

    Integrating Jenkins with GitHub - 15

--- parallel.py
+++ parallel.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-print("Integrating Jenkins with GitHub - 14")
+import jenkins
+
+#server = jenkins.Jenkins('http://10.132.25.49:8080', username='user', password='user')
+#server.build_job('parallel_local')
+#server.build_job('parallel_git_hub')
+#server.build_job('POC')
+
+print("Integrating Jenkins with GitHub - 15")
